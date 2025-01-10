252f093b6255063b6cdaecad932f2bf90db4cf99
gabrielfior <fior.gbm@gmail.com>
2025-01-10 11:07:29 -0300

    Reverting back to ENV projectId

--- python_web3_wallet/frontend/src/index.tsx
+++ python_web3_wallet/frontend/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { WagmiProvider } from "wagmi";
-import { Chain, gnosis, localhost } from "wagmi/chains";
+import { gnosis } from "wagmi/chains";
 import PythonWeb3Wallet from "./PythonWeb3Wallet";
 
 const queryClient = new QueryClient();
@@ -17,6 +17,8 @@ const queryClient = new QueryClient();
 //     default: { http: ['http://127.0.0.1:8545'] },
 //   },
 // } as const satisfies Chain;
+
+console.log('debug', process.env.REACT_APP_DEBUG_VARIABLE);
 
 const config = getDefaultConfig({
   appName: 'app',
